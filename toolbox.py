4ad44e8c974786363e70a8072b43ebc93e0db38f
shantoangelo-byte <shantoangelo@gmail.com>
2026-09-23 09:37:31 +0300

    Add toolbox.py with utility functions

---
+++ toolbox.py
@@ -0,0 +1,21 @@
+
+def double(number):
+    return number * 2
+
+print(double(7))
+
+
+
+def is_pass(score):
+    return score >= 50
+
+
+print(is_pass(80))
+print(is_pass(20))
+
+def greet(name, greeting="hello"):
+    return greeting +"," + name + "!"
+
+
+print(greet("Habii"))
+print(greet("Angelo", "Habari"))
